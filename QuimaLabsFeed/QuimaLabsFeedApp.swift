e8657cd155f69a590eae38a1e4574fe31c207220
Raul Quispe <raul.quispe@live.com>
2023-05-31 21:41:08 -0500

    Initial Commit

---
+++ QuimaLabsFeed/QuimaLabsFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuimaLabsFeedApp.swift
+//  QuimaLabsFeed
+//
+//  Created by Raul on 31/05/23.
+//
+
+import SwiftUI
+
+@main
+struct QuimaLabsFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
