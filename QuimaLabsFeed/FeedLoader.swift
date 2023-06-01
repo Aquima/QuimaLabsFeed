583081cf66b2922463e7aee3242f2347ee84f376
Raul Quispe <raul.quispe@live.com>
2023-05-31 22:08:08 -0500

    (feature) create feedloader protocol boundary

---
+++ QuimaLabsFeed/FeedLoader.swift
@@ -0,0 +1,15 @@
+//
+//  FeedLoader.swift
+//  QuimaLabsFeed
+//
+//  Created by Raul on 31/05/23.
+//
+
+import Foundation
+enum LoadFeedResult {
+    case success([FeedItem])
+    case error(Error)
+}
+protocol FeedLoader {
+    func load(completion:@escaping(LoadFeedResult)->Void)
+}
