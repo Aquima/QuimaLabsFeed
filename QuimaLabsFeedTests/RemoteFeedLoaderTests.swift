ff15519acfd6c3402bce7e9ad84e9bda75e2c395
Raul Samuel Quispe Mamani <raul.quispe@live.com>
2023-08-26 23:51:56 -0500

    give a URL instead of hardcoding in the RemoteFeedLoader

--- QuimaLabsFeedTests/RemoteFeedLoaderTests.swift
+++ QuimaLabsFeedTests/RemoteFeedLoaderTests.swift
@@ -8,12 +8,13 @@
 import XCTest
 class RemoteFeedLoader {
     let client: HTTPClient
-    
-    init(client: HTTPClient) {
+    let url: URL
+    init(url: URL, client: HTTPClient) {
         self.client = client
+        self.url = url
     }
     func load() {
-        client.get(from: URL(string: "asdasd")!)
+        client.get(from: url)
     }
 }
 protocol HTTPClient {
@@ -38,13 +39,15 @@ final class RemoteFeedLoaderTests: XCTestCase {
 
     func testInitDoesNotRequestURL() throws {
         let client = HTTPClientSpy()
-        let _ = RemoteFeedLoader(client: client)
+        let url = URL(string: "https//:www.give.request.com")!
+        let _ = RemoteFeedLoader(url: url, client: client)
         XCTAssertNil(client.requestURL)
     }
     
     func testLoadRequestDataFromURL() {
         let client = HTTPClientSpy()
-        let sut = RemoteFeedLoader(client: client)
+        let url = URL(string: "https//:www.give.request.com")!
+        let sut = RemoteFeedLoader(url: url, client: client)
         sut.load()
         XCTAssertNotNil(client.requestURL)
     }
