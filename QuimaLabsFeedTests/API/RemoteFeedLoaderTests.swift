081650113ce5c5b2605438f20d3f9803dd9ca04d
Raul Samuel Quispe Mamani <raul.quispe@live.com>
2023-09-20 14:42:34 -0500

    guarantee we only receive one error

--- QuimaLabsFeedTests/API/RemoteFeedLoaderTests.swift
+++ QuimaLabsFeedTests/API/RemoteFeedLoaderTests.swift
@@ -31,6 +31,14 @@ final class RemoteFeedLoaderTests: XCTestCase {
         XCTAssertEqual(client.requestedURLs, [url, url])
     }
 
+    func testLoadDeliverErrorOnError() {
+        let (sut, client) = makeSUT()
+        client.error = NSError(domain: "Test", code: 0)
+        var capturedErrors = [RemoteFeedLoader.Error]()
+        sut.load { capturedErrors.append($0) }
+        XCTAssertEqual(capturedErrors, [.connectivity])
+    }
+    // MARK: - Helpers
     private func makeSUT(url: URL = URL(string: "https//:www.give.request.com")!) -> (RemoteFeedLoader, HTTPClientSpy) {
         let client = HTTPClientSpy()
         return (RemoteFeedLoader(url: url, client: client), client)
@@ -45,7 +53,11 @@ final class RemoteFeedLoaderTests: XCTestCase {
 
     private class HTTPClientSpy: HTTPClient {
         var requestedURLs: [URL] = [URL]()
-        func get(from url: URL) {
+        var error: Error?
+        func get(from url: URL, completion: @escaping (Error) -> Void) {
+            if let error = error {
+                completion(error)
+            }
             requestedURLs.append(url)
         }
     }
