66191a7e567e5f06fd735c4a2adf5156c8fdc247
Raul Samuel Quispe Mamani <raul.quispe@live.com>
2023-09-21 03:43:46 -0500

    Deloiver invalid data error on 200 HTTP response with invalid json

--- QuimaLabsFeedTests/API/RemoteFeedLoaderTests.swift
+++ QuimaLabsFeedTests/API/RemoteFeedLoaderTests.swift
@@ -59,6 +59,17 @@ final class RemoteFeedLoaderTests: XCTestCase {
             capturedErrors = []
         }
     }
+
+    func testDeliverErrorOn200HTTPResponseWithInvalidJSON() {
+        let (sut, client) = makeSUT()
+        var capturedErrors = [RemoteFeedLoader.Error]()
+        sut.load { capturedErrors.append($0) }
+        let invalidJson = Data.init("invalid json".utf8)
+        client.complete(withStatusCode: 200, data: invalidJson)
+
+        XCTAssertEqual(capturedErrors, [.invalidData])
+        capturedErrors = []
+    }
     // MARK: - Helpers
     private func makeSUT(url: URL = URL(string: "https//:www.give.request.com")!) -> (RemoteFeedLoader, HTTPClientSpy) {
         let client = HTTPClientSpy()
@@ -86,12 +97,12 @@ final class RemoteFeedLoaderTests: XCTestCase {
             messages[0].completion(.failure(error))
         }
 
-        func complete(withStatusCode code: Int, at index: Int = 0) {
+        func complete(withStatusCode code: Int, data: Data = Data(), at index: Int = 0) {
             let response = HTTPURLResponse(url: requestedURLs[index],
                                            statusCode: code,
                                            httpVersion: nil,
                                            headerFields: nil)!
-            messages[index].completion(.success(response))
+            messages[index].completion(.success(data, response))
         }
     }
 }
